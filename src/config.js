6ec68356da84b5fea42e2b8026ce8ba940d91d1f
saniaforever <saniamukadam101@gmail.com>
2025-02-11 12:08:31 +0530

    Update config.js
    Updated Render Backend

--- src/config.js
+++ src/config.js
@@ -1,7 +1,6 @@
-let BASE_URL = "https://royalcomplains-backend.onrender.com"; 
-
+let BASE_URL = "https://royalcomplains-backend.onrender.com/";
 if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
   BASE_URL = "http://localhost:4000/";
 }
 
-export { BASE_URL
+export { BASE_URL };
